45eadc88910f7ba33f0239832e1973e27e2c3fb3
Zhal100 <106669776+Zhal100@users.noreply.github.com>
2022-06-01 21:42:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.ownerNumberr = "6283816171576"
+global.ownerbot = ['6283816171576@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
+global.ownername = "ZhaaLL ×፝֟͜×"
+global.botname = "ZhaaLLBot"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  ZhaaLLBot  」\nCreated By ᴹᴿ᭄ ZhaaLLcོ ×፝֟͜×"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
